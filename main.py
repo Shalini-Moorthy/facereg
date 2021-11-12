57aa7319b5df2b8c84a0f2dbfb20880eb37c8786
shalini <shalini1152001@gmail.com>
2021-11-12 16:14:27 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+from flask import Flask,render_template,request
+import pymysql.cursors
+import cv2
+import collectsample
+import recognizer
+import train
+
+connection=pymysql.connect(host="localhost",user="root",port=3306,password="root",db="secureot",cursorclass=pymysql.cursors.DictCursor)
+app=Flask(__name__)
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+@app.route('/form_welcome',methods=['post','get'])
+def welcome():
+    email=request.form['email']
+    password=request.form['password']
+    with connection.cursor() as cursor:
+        sqlQuerys = "select * from userdetails"
+        cursor.execute(sqlQuerys)
+        result = cursor.fetchall()
+        connection.commit()
+        print(result)
+        emaillist=[]
+        for i in result:
+            emaillist.append(i['email'])
+        print(emaillist)
+
+        if email in emaillist:
+            a=emaillist.index(email)
+            print(emaillist,a,result[a]['password'],result[a]['email'])
+            name=result[a]['name']
+            if password==result[a]['password']:
+
+               return render_template("Welcome.html",name=name)
+               connection.close()
+            else:
+                return render_template("login.html",message="Invalid password")
+        else:
+            return render_template("login.html",message="User doesn't exist")
+
+
+
+@app.route('/form_gotosignup',methods=['post','get'])
+def signup():
+    return render_template("SignUp.html")
+
+@app.route('/form_signupdetails',methods=['post','get'])
+def insertDetailsIntoDB():
+    name= request.form['username']
+    email=request.form['email']
+    password=request.form['password']
+    try:
+        with connection.cursor() as cursor:
+
+            sqlQuery = "insert into userdetails (name,email,password) VALUES (%s,%s,%s)"
+            cursor.execute(sqlQuery, ( name,email, password))
+            connection.commit()
+    finally:
+        connection.close()
+
+    return render_template("NewUserWelcome.html",name=name)
+@app.route('/form_collectsampless',methods=['post','get'])
+def collectt():
+    collectsample.collect()
+    train.trainfun()
+    return render_template("MessageSampleCollected.html")
+@app.route('/form_gotowelcomepage',methods=['post','get'])
+def backtowelcome():
+    return render_template("Welcome.html")
+@app.route('/form_recognition',methods=['post','get'])
+def recognition():
+    recognizer.recognize()
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ train.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile , join
+def trainfun():
+    data_path='C:/Users/shalini/Desktop/sotproject/faces/'
+
+    onlyfiles=[f for f in listdir(data_path) if isfile(join(data_path,f))]
+    training_data,labels=[],[]
+    for i , files in enumerate(onlyfiles):
+        image_path=data_path+onlyfiles[i]
+        images=cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+        training_data.append(np.asarray(images,dtype=np.uint8))
+        labels.append(i)
+    labels=np.asarray(labels,dtype=np.int32)
+    model=cv2.face.LBPHFaceRecognizer_create()
+    model.train(np.asarray(training_data),np.asarray(labels))
+    model.save('C:/Users/shalini/Desktop/sotproject/models/model'+str(1)+'.yml')
+    print("Model Training Complete")
